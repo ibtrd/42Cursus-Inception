8103121bdde59c6828fd10ba0bee09700477caf2
ibertran <128724498+ibtrd@users.noreply.github.com>
2024-11-19 19:19:11 -0600

    fix: remove mariadb useless user

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -3,9 +3,7 @@ set -e
 
 export MYSQL_DATABASE=$(cat /run/secrets/mysql_database)
 export MYSQL_ROOT_PASSWORD=$(cat /run/secrets/mysql_root_password)
-export MYSQL_ADMIN=$(cat /run/secrets/mysql_admin)
-export MYSQL_ADMIN_PASSWORD=$(cat /run/secrets/mysql_admin_password)
 export MYSQL_USER=$(cat /run/secrets/mysql_user)
-export MYSQL_USER_PASSWORD=$(cat /run/secrets/mysql_user_password)
+export MYSQL_PASSWORD=$(cat /run/secrets/mysql_password)
 envsubst < /run/initdb.sql | sponge /run/initdb.sql
 exec $@ --init-file=/run/initdb.sql
