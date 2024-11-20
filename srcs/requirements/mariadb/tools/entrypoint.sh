b5f3ee1193d176e5d66f4b1d92f86bf6eb00aff3
ibertran <128724498+ibtrd@users.noreply.github.com>
2024-11-19 22:30:10 -0600

    feat: prevent database initialization on container restart

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 set -e
 
-export MYSQL_DATABASE=$(cat /run/secrets/mysql_database)
-export MYSQL_ROOT_PASSWORD=$(cat /run/secrets/mysql_root_password)
-export MYSQL_USER=$(cat /run/secrets/mysql_user)
-export MYSQL_PASSWORD=$(cat /run/secrets/mysql_password)
-envsubst < /run/initdb.sql | sponge /run/initdb.sql
-exec $@ --init-file=/run/initdb.sql
+if [ ! -d /var/lib/mysql/mysql ];
+then
+	mariadb-install-db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
+	export MYSQL_DATABASE=$(cat /run/secrets/mysql_database)
+	export MYSQL_ROOT_PASSWORD=$(cat /run/secrets/mysql_root_password)
+	export MYSQL_USER=$(cat /run/secrets/mysql_user)
+	export MYSQL_PASSWORD=$(cat /run/secrets/mysql_password)
+	envsubst < /run/initdb.sql | sponge /run/initdb.sql
+	exec $@ --init-file=/run/initdb.sql --port=3306
+else
+	exec $@
+fi
